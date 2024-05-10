eaba486dbf53ec91d9b27d575328a54428b547ef
bluextx <bluextchannel@gmail.com>
2024-05-10 23:13:23 +0300

    Changed redocly cli output format

--- documentation/lint-openapi.ps1
+++ documentation/lint-openapi.ps1
@@ -1,1 +1,4 @@
-docker run -v ${PWD}:/docs --workdir /docs redocly/cli:1.11.0 lint openapi.yaml
+param  (
+    $outputFormat = "codeframe"
+)
+docker run -v ${PWD}:/docs --workdir /docs redocly/cli:1.11.0 lint --format $outputFormat openapi.yaml
